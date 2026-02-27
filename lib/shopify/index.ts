4ff55b7a6938ee0cb87691ad29ba4f594a61eaa7
markkmagedd <markmagedamazon@gmail.com>
2026-02-27 05:43:27 +0200

    fix: revert cache to no-store so pages always serve fresh content

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -28,7 +28,7 @@ async function shopifyFetch<T>({
   query,
   variables,
   headers,
-  cache = "force-cache",
+  cache = "no-store",
   tags,
 }: {
   query: string;
@@ -57,7 +57,7 @@ async function shopifyFetch<T>({
         ...(variables && { variables }),
       }),
       cache,
-      next: { revalidate: 900, ...(tags && tags.length > 0 ? { tags } : {}) },
+      next: { revalidate: 0, ...(tags && tags.length > 0 ? { tags } : {}) },
     });
 
     const body = await result.json();
